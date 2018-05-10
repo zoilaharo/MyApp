afdbd74a802817e682b2ff9b514426726ee08307
Zoila Haro Pena <15178339+zoilaharo@users.noreply.github.com>
2018-05-09 21:39:44 -0700

    Assig 5 - first interation of test cases

--- app/src/main/java/com/example/zoilaharo/myapp/MatchesContentFragment.java
+++ app/src/main/java/com/example/zoilaharo/myapp/MatchesContentFragment.java
@@ -1,27 +1,26 @@
 package com.example.zoilaharo.myapp;
 
-/*
- * Copyright (C) 2015 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Provides UI for the view with Cards.
@@ -30,6 +29,60 @@ public class MatchesContentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.item_matches, null);
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        // Set padding for Tiles
+        int tilePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
+        recyclerView.setPadding(tilePadding, tilePadding, tilePadding, tilePadding);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView picture;
+        public TextView name;
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_matches, parent, false));
+            picture = itemView.findViewById(R.id.matches_image);
+            name = itemView.findViewById(R.id.matches_title);
+        }
+    }
+    /**
+     * Adapter to display recycler view.
+     */
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of List in RecyclerView.
+        private static final int LENGTH = 18;
+        private final String[] mPlaces;
+        private final Drawable[] mPlacePictures;
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mPlaces = resources.getStringArray(R.array.places);
+            TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+            mPlacePictures = new Drawable[a.length()];
+            for (int i = 0; i < mPlacePictures.length; i++) {
+                mPlacePictures[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.picture.setImageDrawable(mPlacePictures[position % mPlacePictures.length]);
+            holder.name.setText(mPlaces[position % mPlaces.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return LENGTH;
+        }
     }
 }
--- app/src/main/java/com/example/zoilaharo/myapp/ProfileContentFragment.java
+++ app/src/main/java/com/example/zoilaharo/myapp/ProfileContentFragment.java
@@ -2,7 +2,9 @@ package com.example.zoilaharo.myapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.SyncStateContract;
 import android.support.annotation.Nullable;
+import android.support.constraint.solver.widgets.ConstraintAnchor;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.support.v4.app.FragmentTransaction;
+import android.widget.Toast;
 
 /**
  * Provides UI for the view with List.
@@ -17,17 +20,21 @@ import android.support.v4.app.FragmentTransaction;
 public class ProfileContentFragment extends Fragment {
     private EditText nameTxt, usernameTxt, ageTxt;
     private UserAccount.Operation operation;
+    static final String KEY_TEXTVIEW_TEXT = "txtv_text";
+    private int score;
+
+    TextView username_View, name_View, email_View, age_View,occupation_View,description_View;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.item_profile, container, false);
-        TextView username_View = view.findViewById(R.id.username_acct_profile);
-        TextView name_View = view.findViewById(R.id.name_acct_profile);
-        TextView email_View = view.findViewById(R.id.email_acct_profile);
-        TextView age_View = view.findViewById(R.id.age_acct_profile);
-        TextView occupation_View = view.findViewById(R.id.occupation_acct_profile);
-       TextView description_View = view.findViewById(R.id.description_acct_profile);
+        username_View = view.findViewById(R.id.username_acct_profile);
+        name_View = view.findViewById(R.id.name_acct_profile);
+        email_View = view.findViewById(R.id.email_acct_profile);
+        age_View = view.findViewById(R.id.age_acct_profile);
+        occupation_View = view.findViewById(R.id.occupation_acct_profile);
+        description_View = view.findViewById(R.id.description_acct_profile);
 
         if (operation != null) {
             String name = operation.name;
@@ -37,18 +44,38 @@ public class ProfileContentFragment extends Fragment {
             String occupation = operation.occupation;
             String description = operation.description;
 
-            username_View.setText(username);
             name_View.setText(name);
+            username_View.setText(username);
             email_View.setText(email);
             age_View.setText(age);
             occupation_View.setText(occupation);
             description_View.setText(description);
+
+        }
+
+        if (savedInstanceState != null) {
+            name_View.setText(savedInstanceState.getString("R.string.profFrag_name", getString(R.string.name)));
+            username_View.setText(savedInstanceState.getString("R.string.profFrag_username", getString(R.string.username)));
+            email_View.setText(savedInstanceState.getString("R.string.profFrag_email", getString(R.string.email)));
+            age_View.setText(savedInstanceState.getString("R.string.profFrag_age", getString(R.string.age)));
+            occupation_View.setText(savedInstanceState.getString("R.string.profFrag_occupation", getString(R.string.occupation)));
+            description_View.setText(savedInstanceState.getString("R.string.profFrag_description", getString(R.string.description)));
         }
 
         return view;
     }
     public void setOperation(UserAccount.Operation operation) {
         this.operation = operation;
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("R.string.profFrag_name", name_View.getText().toString());
+        outState.putString("R.string.profFrag_username", username_View.getText().toString());
+        outState.putString("R.string.profFrag_email", email_View.getText().toString());
+        outState.putString("R.string.profFrag_age", age_View.getText().toString());
+        outState.putString("R.string.profFrag_description", description_View.getText().toString());
+        outState.putString("R.string.profFrag_occupation", occupation_View.getText().toString());
     }
 
    }
